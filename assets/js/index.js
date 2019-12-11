e9e6b6363376329a1f3592a9d3acebf514fab539
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-12-11 15:35:56 -0600

    Adjusted prompt within JS.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,26 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+function askQuestion() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "How big is it?",
+        name: "dave"
+      },
+      {
+        type: "input",
+        message: "What is your nieces name?",
+        name: "niecesName"
+      },
+      {
+        type: "input",
+        message: "Why are you late?",
+        name: "late"
+      }
+    ])
+    .then(function(response) {
+      console.log(response);
+    });
+}
